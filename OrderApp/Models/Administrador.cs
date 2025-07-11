89eab73224ad065c68e75e777980ace72f11c95d
PLuiza12 <pluiza64@gmail.com>
2025-07-11 11:10:28 -0300

    Validação de dados para a entidade Administrador

--- OrderApp/Models/Administrador.cs
+++ OrderApp/Models/Administrador.cs
@@ -7,10 +7,17 @@ namespace OrderApp.Models
         [Key]
         public int AdmId { get; set; }
 
-        public string AdmNome { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome pode ter no máximo 100 caracteres.")]
 
-        public string Telefone { get; set; }
+        public string? AdmNome { get; set; }
 
+        [Phone(ErrorMessage = "Número de telefone inválido.")]
+
+        public string? Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
         public string? Email { get; set; }
     }
 }
+
